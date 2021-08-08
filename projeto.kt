c26d12fab285001b0e86bdf753bebcd77a871d4d
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-08-08 14:14:09 -0400

    new function

--- projeto.kt
+++ projeto.kt
@@ -1,5 +1,14 @@
 fun main(args: Array<String>) {
     println(max(2,4))
+    println(min(2,4))
 }
 
+/*
+*    retorna o maior valor
+*/
 fun max(x: Int, y: Int) = if(x > y) x else y
+
+/*
+*    retorna o menor valor
+*/
+fun min(a: Int, b: Int) = if(a < b) a else b
